cb6ac38dbe41c0a0edb53fda0dc1c3734100feb7
Chris Rohit Brendan <rohit.brendan@gmail.com>
2020-06-16 12:37:53 +0530

    Updated stacked.dart

--- packages/stacked/lib/stacked.dart
+++ packages/stacked/lib/stacked.dart
@@ -2,6 +2,7 @@ library stacked;
 
 export 'src/base_view_models.dart';
 export 'src/reactive_service_mixin.dart';
+export 'src/selector_view_model_widget.dart';
 export 'src/view_model_builder.dart';
 export 'src/view_model_builder_widget.dart';
 export 'src/view_model_widget.dart';
